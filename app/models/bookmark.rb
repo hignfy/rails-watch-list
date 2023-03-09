552f160ee16f4aa3dc7372dd0d0259d2f70476d5
Emma McKeown <diamond_rain@hotmail.co.uk>
2023-03-09 17:57:28 -0300

    nope

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -3,6 +3,7 @@ class Bookmark < ApplicationRecord
   belongs_to :list
 
   validates :movie_id, :list_id, presence: true
-  validates :movie_id, uniqueness: { scope: :list_id }
+  validates :movie_id, uniqueness: { scope: :list_id, message: 'This movie is already on the list' }
+  # the movie is unique with the list
   validates :comment, length: { minimum: 6 }
 end
